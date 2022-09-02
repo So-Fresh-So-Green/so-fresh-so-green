9d82697dedaa74d3db28045d70aa4acd4a06ff34
ian.moss <ianmsieg@gmail.com>
2022-09-02 11:56:25 -0500

    added basic serverside files

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+const bcrypt = require('bcrypt')
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+    trim: true,
+  },
+  thoughtAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+      },
+    },
+  ],
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ server/models/index.js
@@ -0,0 +1,3 @@
+const Thought = require('./User');
+
+module.exports = { Thought };
