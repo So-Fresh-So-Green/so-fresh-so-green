9ec63d087dbcdf8ab539b6ace4e9a55b85cd66c0
ian.moss <ianmsieg@gmail.com>
2022-09-07 17:00:18 -0500

    exported post and comment from index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,8 @@ const User = require('./User');
 const Product = require('./Product');
 const Category = require('./Category');
 const Order = require('./Order');
-const Plant = require('./Plant')
+const Plant = require('./Plant');
+const Post = require('./Post');
+const Comment = require('./Comment')
 
-module.exports = { User, Product, Category, Order, Plant };
+module.exports = { User, Product, Category, Order, Plant, Post, Comment };
