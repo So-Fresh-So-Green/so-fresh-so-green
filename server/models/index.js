613bd97eacaacb2fe7763903e9b6d1ec7b41af5a
Ian Sieg <98928887+ian-sieg@users.noreply.github.com>
2022-09-06 21:32:25 -0500

    Delete server directory

--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require('./User');
-const Plant = require('./Plant');
-const Post = require('./Post');
-const Comment = require('./Comment');
-const Sale = require('./Sale')
-
-module.exports = { User, Plant, Post, Comment, Sale };
