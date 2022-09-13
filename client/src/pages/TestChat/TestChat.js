174b2045cc5869acd0b30ef0bea94ebb2c65f3b1
kkmusau <kkmusau@gmail.com>
2022-09-12 22:23:20 -0500

    added a test chat

--- client/src/pages/TestChat/TestChat.js
+++ client/src/pages/TestChat/TestChat.js
@@ -4,18 +4,7 @@ import { io } from "socket.io-client";
  // Connect to Socket.io
 const socket = io("http://localhost:8800");
 
-useEffect(() => {
-  socket.current = io("ws://localhost:8800");
-  socket.current.emit("new-user-add", user._id);
-  socket.current.on("get-users", (users) => {
-    setOnlineUsers(users);
-  });
-}, [user]);
 
-
-socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  });
 
 const TestChat = () =>
   <main id='chatMain'>
