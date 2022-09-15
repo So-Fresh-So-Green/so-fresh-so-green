7cd4e87c6021aaba6c91b76246cb5fdbadb04f31
ian.moss <ianmsieg@gmail.com>
2022-09-14 21:38:14 -0500

    exporting prof playground now

---
+++ client/src/pages/ProfilePlayground.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { useMutation } from '@apollo/client';
+
+// TODO: return either an exterior or interior profile view depending on which user is viewing it
+
+export default function ProfilePlayground() {
+    <div>Hi</div>
+}
